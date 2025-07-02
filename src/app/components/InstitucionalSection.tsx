4901f5292439d7fa366b961096ca297a6c9ac456
Gabriel da Silva França <gabrielfranca250@gmail.com>
2025-07-02 12:22:30 -0300

    institucional

---
+++ src/app/components/InstitucionalSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export function InstitucionalSection(){
+    return (
+        <div>
+            xxx
+        </div>
+    );
+}
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+import React from "react";
 import Image from "next/image";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import React from "react";
 import { Button } from "./components/Button";
 import { Section } from "./components/Section";
 import { Card } from "./components/Card";
@@ -31,10 +31,14 @@ export default function Home() {
 
         <Section title="Institucional">
           <Card 
-            title="Nossa Missão"
-            content={`Somos comprometidos com o ensino, pesquisa e extensão em **Ciência da Computação** e **Sistemas de Informação** na UFSM.`}
+            title="Sobre"
+            content={`### Boas-vindas ao Portal da Informática UFSM
+              Bem-vindo ao Portal do Departamento de Informática da UFSM! Somos um centro de excelência em ensino, pesquisa e extensão, dedicados a aprimorar a experiência do usuário por meio de uma interface moderna, intuitiva e acessível. Nossa missão é promover o desenvolvimento científico e tecnológico na área de Computação, formando profissionais éticos, criativos e comprometidos com a solução de problemas da sociedade através da tecnologia.
+              Inspirados nas melhores práticas de sites universitários, como o da Informática da UFRGS, buscamos mesclar a seriedade institucional com a paixão pelo universo da tecnologia, garantindo que as informações principais estejam acessíveis de forma rápida e clara para estudantes, docentes e visitantes.`}
             />
         </Section>
+
+        
 
         <Button label="Saiba Mais >" />
       </main>
